5513fb340c3e3a058e355247c44f64c5d1f58861
yxmmy <alexisdervax@gmail.com>
2024-04-10 14:57:23 +0200

    Co-authored-by: Haroldinhoo <Haroldinhoo@users.noreply.github.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-
+//les durees sont en secondes.
 
 let recettes = [{
     plat: 'Riz au curry',
@@ -6,10 +6,10 @@ let recettes = [{
     dureePreparation: 900,
     dureeRepos: 0,
     dureeCuisson: 900,
-    nbPersonnes: 4,
+    nbPersonnes: 1,
     ustensiles:  ['passoire', 'poele', 'cuillere en bois'],
     difficulte: 'Facile.',
-    dureeTotale: 10,
+    dureeTotale: 1800,
 },
 
 {
@@ -18,13 +18,56 @@ let recettes = [{
     dureePreparation: 1500,
     dureeRepos: 0,
     dureeCuisson: 3300,
-    dureeTotale: 10,
+    dureeTotale: 4800,
     nbPersonnes: 1,
     ustensiles:  ['1 casserole', '1 four', '3 poeles', 'planche a decouper'],
     difficulte: 'Facile',
 },
 
 {
+    plat: "Dombre aux crevettes",
+    ingredients: ['oignon', 'poivre', 'sel', '3 cuillères à soupe d huile de tournesol', '800g de crevettes','500g de farine', 'une grande boite de tomates pelees', '1 cuilleres a cafe', '1 gouse d ail'],
+    dureePreparation: 1900,
+    dureeRepos: 0,
+    dureeCuisson: 2400,
+    dureeTotale: 4300,
+    nbPersonnes: 5,
+    ustensiles: ['1 bol', '1 couvercle', '1 couteau', '1 marmite', '1 balance'],
+    difficulte: 'Moyenne',
+},
+
+{
+    plat: 'Pates à la carbonara',
+    ingredients: ['1 oignon', 'poivre', 'sel', '125g de lardons','25cl de creme fraiche', '250g de pates', '1 jaune d oeuf'],
+    dureePreparation: 600,
+    dureeRepos: 0,
+    dureeCuisson: 600,
+    dureeTotale: 1200,
+    nbPersonnes: 2 ,
+    ustensiles: ['1 passoire, 1 couteau', '3 poeles', '1 saladier', '1 cuillere en bois', '1 casserole', '1 plat'],
+    difficulte: 'Facile',
+},
+{
+    plat: "Couscous poulet et merguez facile",
+    ingredients: ['2 oignons', 'poivre', 'sel', '3 cuillères à soupe d huile de tournesol', '800g de crevettes','2 cuillièr de farine', 'une grande boite de tomates pelees', '1 cuillères a cafe', '1 gouse d ail'],
+    dureePreparation: 1900,
+    dureeRepos: 0,
+    dureeCuisson: 2400,
+    dureeTotale: 10,
+    nbPersonnes: 6,
+    ustensiles: ['1 bol', '1 couvercle', '1 couteau', '1 marmite', '1 balance'],
+    difficulte: 'Moyenne',
+},{
+    plat: "Boeuf Bourguignon rapide",
+    ingredients: ['oignon', 'poivre', 'sel', '250g de champignons de Paris (en boite)','66cl de vin rouge','50g de beurre ou 3 cuillieres a soupe d huile', '100g de lardons','500g de farine', '800g de boeuf pour bourguignon', '1 bouquet garni', '1 gouse d ail'],
+    dureePreparation: 600,
+    dureeRepos: 0,
+    dureeCuisson: 3600,
+    dureeTotale: 4200,
+    nbPersonnes: 6,
+    ustensiles: ['1 mixeur', '1 cocotte', '1 couteau', '1 pinceaux', '1 cuillere en bois','1 mijoteuse electrique','1 balance de cuisine'],
+    difficulte: 'Facile',
+},{
     plat: "Dombre aux crevettes",
     ingredients: ['oignon', 'poivre', 'sel', '3 cuillères à soupe d huile de tournesol', '800g de crevettes','500g de farine', 'une grande boite de tomates pelees', '1 cuillères a cafe', '1 gouse d ail'],
     dureePreparation: 1900,
@@ -35,22 +78,6 @@ let recettes = [{
     ustensiles: ['1 bol', '1 couvercle', '1 couteau', '1 marmite', '1 balance'],
     difficulte: 'Moyenne',
 },
-
-{
-    plat: 'Pâtes à la carbonara',
-    ingredients: ['oignon', 'poivre', 'sel', '3 cuillères à soupe d huile de tournesol', '800g de crevettes','500g de farine', 'une grande boite de tomates pelees', '1 cuillères a cafe', '1 gouse d ail'],
-    dureePreparation: 1900,
-    dureeRepos: 0,
-    dureeCuisson: 2400,
-    dureeTotale: 10,
-    nbPersonnes: 2 ,
-    ustensiles: ['1 bol', '1 couvercle', '1 couteau', '1 marmite', '1 balance'],
-    difficulte: 'Facile',
-},
-
-{
-
-}
 ]
 
 function filterItems(recettes, key, value) {
@@ -71,6 +98,3 @@ const fastRecipes = filterItems(recettes, 'dureeCuisson', 0);
 
 console.log("Recettes faciles :", easyRecipes);
 console.log("Recettes rapides :", fastRecipes);
-
-
-
