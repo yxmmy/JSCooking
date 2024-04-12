9a77a6eeee133ae0391e1b4c05efb4392bbfb295
yxmmy <alexisdervax@gmail.com>
2024-04-12 16:02:27 +0200

    la modif

--- assets/js/script.js
+++ assets/js/script.js
@@ -151,23 +151,3 @@ const fastRecipes = filterItems(recettes, 'dureeCuisson', 0);
 
 console.log("Recettes faciles :", easyRecipes);
 console.log("Recettes rapides :", fastRecipes);
-
-setTimeout(() => {
-    console.log("this is the first message");
-  }, 5000);
-  setTimeout(() => {
-    console.log("this is the second message");
-  }, 3000);
-  setTimeout(() => {
-    console.log("this is the third message");
-  }, 1000);
-
-
-const mobileNav = document.querySelector(".hamburger");
-const navbar = document.querySelector(".menubar");
-
-const toggleNav = () => {
-  navbar.classList.toggle("active");
-  mobileNav.classList.toggle("hamburger-active");
-};
-mobileNav.addEventListener("click", () => toggleNav());
