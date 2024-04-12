b3160792c22afbcd22c3b9223108143b1a2ccdc8
Haroldinhoo <hkancel@myges.fr>
2024-04-12 15:18:31 +0200

    qui indique
    qui indique

--- assets/js/script.js
+++ assets/js/script.js
@@ -162,3 +162,12 @@ setTimeout(() => {
     console.log("this is the third message");
   }, 1000);
 
+  const mobileNav = document.querySelector(".hamburger");
+  const navbar = document.querySelector(".menubar");
+  
+  const toggleNav = () => {
+    navbar.classList.toggle("active");
+    mobileNav.classList.toggle("hamburger-active");
+  };
+  mobileNav.addEventListener("click", () => toggleNav());
+  
